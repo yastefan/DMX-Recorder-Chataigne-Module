dfdf2b173462d75794f35e054992a4ddb717e448
yastefan <stefanschmidt@posteo.org>
2022-08-30 16:49:13 +0200

    hm

--- recorder.js
+++ recorder.js
@@ -1,9 +1,20 @@
+var recordFile = "recordings/record_" + util.getTimestamp() + ".csv";
+var inputBuffer = [];
+var dataToWrite = "";
+
 function init() {
   script.log("dmx recorder module loaded");
+  util.writeFile(recordFile, data, true);
+}
+
+function dmxEvent(values) {
+  script.log(values.length);
+  inputBuffer.push(values);
 }
 
 function record(param) {
 }
 
-function record(param) {
+function replay(param) {
 }
+
